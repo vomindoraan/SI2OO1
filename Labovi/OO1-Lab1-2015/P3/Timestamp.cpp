52b28416bd2d1779e2f02aa2d7ab0d065dea2f9c
Konstantin Đorđević <vomindoraan@gmail.com>
2017-10-23 11:39:44 +0200

    Replace std::div in Lab1 P3 with a more readable alternative

--- Labovi/OO1-Lab1-2015/P3/Timestamp.cpp
+++ Labovi/OO1-Lab1-2015/P3/Timestamp.cpp
@@ -1,7 +1,5 @@
 ﻿#include "Timestamp.h"
 
-#include <cstdlib> // std::div
-
 // Sve metode koje su def. u samoj klasi, u header fajlu, su automatski inline
 // Neke složenije metode, koje npr. imaju petlje ili pozive f-ja ne treba pisati
 // u klasi, već samo navesti njihov potpis, a implementirati ih u .cpp fajlu
@@ -9,16 +7,10 @@
 // Neophodno je proširiti plus sa Timestamp:: da bi se odnosilo na metod te klase
 Timestamp Timestamp::plus(const Timestamp& other) const
 {
-	Timestamp res;
-
-	auto d = std::div(s_ + other.s_, 60); // d = količnik i ostatak (kao struct)
-	res.s_ = d.rem;                       // rem (ostatak) je preostali br. sek.
-
-	d = std::div(m_ + other.m_ + d.quot, 60); // quot (količnik) ulazi u minute
-	res.m_ = d.rem;
-
-	res.h_ = h_ + other.h_ + d.quot;      // Isto za minute i sate
-	return res;
+	auto s = s_ + other.s_;
+	auto m = m_ + other.m_ + s / 60;
+	auto h = h_ + other.h_ + m / 60;
+	return Timestamp(h, m % 60, s % 60);
 }
 
 // Ovde treba proširiti i CmpResult sa Timestamp:: jer je taj tip član klase
